804b34b0abbfd2faba6783cda85c95bca7da36b3
donghoony <aru0504@naver.com>
2024-04-26 17:14:37 +0900

    feat: 콘솔용 인메모리 DB 구현체 작성

---
+++ src/main/java/roomescape/console/repository/ReservationInMemoryRepository.java
@@ -0,0 +1,44 @@
+package roomescape.console.repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import roomescape.core.domain.Reservation;
+import roomescape.core.domain.ReservationRepository;
+
+@Repository
+public class ReservationInMemoryRepository implements ReservationRepository {
+
+    private final AtomicLong idCounter;
+    private final Map<Long, Reservation> reservations;
+
+    public ReservationInMemoryRepository() {
+        this.idCounter = new AtomicLong(1);
+        this.reservations = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public Reservation addReservation(Reservation reservation) {
+        long id = idCounter.getAndIncrement();
+        Reservation savedReservation = reservation.withId(id);
+        reservations.put(id, savedReservation);
+        return savedReservation;
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return List.copyOf(reservations.values());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        reservations.remove(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        reservations.clear();
+    }
+}
---
+++ src/main/java/roomescape/console/repository/TimeSlotInMemoryRepository.java
@@ -0,0 +1,58 @@
+package roomescape.console.repository;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import roomescape.core.domain.TimeSlot;
+import roomescape.core.domain.TimeSlotRepository;
+
+@Repository
+public class TimeSlotInMemoryRepository implements TimeSlotRepository {
+
+    private final AtomicLong idCounter;
+    private final Map<Long, TimeSlot> timeSlots;
+
+    public TimeSlotInMemoryRepository() {
+        this.idCounter = new AtomicLong(1);
+        this.timeSlots = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public TimeSlot addTimeSlot(TimeSlot timeSlot) {
+        long id = idCounter.getAndIncrement();
+        TimeSlot savedTimeSlot = timeSlot.withId(id);
+        timeSlots.put(id, savedTimeSlot);
+        return savedTimeSlot;
+    }
+
+    @Override
+    public List<TimeSlot> findAllOrderByTimeAscending() {
+        return List.copyOf(timeSlots.values());
+    }
+
+    @Override
+    public Optional<TimeSlot> findById(Long id) {
+        return Optional.ofNullable(timeSlots.get(id));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        timeSlots.remove(id);
+    }
+
+    @Override
+    public boolean existsByTime(LocalTime time) {
+        return timeSlots.values()
+                .stream()
+                .anyMatch(timeSlot -> timeSlot.hasTimeOf(time));
+    }
+
+    @Override
+    public void deleteAll() {
+        timeSlots.clear();
+    }
+}
